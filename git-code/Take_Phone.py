46964bcd70468534b981cc09d84af15148ae5268
sullss <13905602779@163.com>
2018-09-28 20:59:20 +0800

    web file take

--- git-code/Take_Phone.py
+++ git-code/Take_Phone.py
@@ -13,23 +13,40 @@ def getHtml(url):
     html = page.read()
     return html.decode('gbk')#这里通过查看网页源码的编码格式charset格式为GBK，将源码重新进行解码操作
 
+# '(?=.*\d)(?=.*[a-zA-Z])(?<=主屏分辨率：)\d\w+'
 def getphoneFB(html):
-    phonere = re.findall('(?=.*\d)(?=.*[a-zA-Z])(?<=主屏分辨率：)\d\w+',html)
+    regStr = '主屏分辨率：\\s*(.*?)像素'
+    patter = re.compile(regStr)
+    phonere = re.findall(patter,html)
     return phonere
 #打开机型文件，放到列表中
 file_path = 'D:\pythonCode\phone.txt'
-uurl_list = []
-phone_msg_list = []
+phone_msg_list = {}
 with open(file_path) as file_object:
     file = file_object.read().splitlines()
-#将所有地址组合好后，放到地址列表内等待查询使用
-for line in file:
-    uurl = "http://search.zol.com.cn/s/all.php?kword="+ line  #组合出所有机型的查询地址
-    uurl_list.append(uurl)
-#使用列表中的地址，依次打开后将指定网页信息爬去下来，并与机型对应成字典
-for url_line in uurl_list:
-    print(getphoneFB(getHtml(url_line)))
-
-
-
+    x = 0
+    while x < len(file):
+        file[x] = "http://search.zol.com.cn/s/all.php?kword=" + file[x]
+        try:
+            phone_FB = getphoneFB(getHtml(file[x]))
+            phone_msg_list[file[x]] = phone_FB
+        except Exception:
+            pass
+        continue
+        x += 1
 print(phone_msg_list)
+
+#
+# #将所有地址组合好后，放到地址列表内等待查询使用
+# for line in file:
+#     uurl = "http://search.zol.com.cn/s/all.php?kword="+ line  #组合出所有机型的查询地址
+#     try:
+#         phone_FB = getphoneFB(getHtml(uurl))
+#         phone_msg_list.append(phone_FB)
+#     except Exception:
+#         pass
+#     continue
+#
+# print(phone_msg_list)
+
+#使用列表中的地址，依次打开后将指定网页信息爬去下来，并与机型对应成字典
